fd405d2b6ec276e7e8417014016fe0eae4aef930
Ciao Chung <ciao0958@gmail.com>
2018-07-09 22:31:45 +0800

    add sass loader

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,9 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+export default {}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="sass" type="text/sass" scoped></style>
